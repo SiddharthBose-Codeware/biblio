babad7bc06a0576da75421b199ef167d6680258e
Siddharth Bose <siddharthbose72@gmail.com>
2018-10-10 08:14:11 -0500

    Adding MySQL DB.

--- biblio/biblio/settings.py
+++ biblio/biblio/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'biblio'
 ]
 
 MIDDLEWARE = [
@@ -75,9 +76,20 @@ WSGI_APPLICATION = 'biblio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'biblio',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '',
+        'OPTIONS': {
+            'sql_mode': 'traditional'
+        }
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
 }
 
 
